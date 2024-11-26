cb68e482e820f878fba3016934dd4e83d5a94c4c
Hicham G <hichem_gouia@hotmail.com>
2024-11-26 14:48:11 +0100

    update config.jswith client secret

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-3_FJKalN4Ax', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '450r26fkfi6urao6d2085fn78n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        client_secret: '1c1kevo2uqj5a6vp4mqge5ueonfveo7vr1nr6dcmfbhpiltle7m0',
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
